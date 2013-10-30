26b3c5d579ef0ae7068fe8c04e84b940dcebec9c
Vladimir Zaytsev <zaytsev@usc.edu>
2013-10-30 00:28:01 -0700

    Updated test data decompression script

--- run_decompress.sh
+++ run_decompress.sh
@@ -17,3 +17,7 @@
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/es/sentence.xz > test_data/tiny/es/sentence.txt
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/es/sentence.xz > test_data/medium/es/sentence.txt
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/es/sentence.xz > test_data/large/es/sentence.txt
+
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/es/document.xz > test_data/tiny/es/document.txt
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/es/document.xz > test_data/medium/es/document.txt
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/es/document.xz > test_data/large/es/document.txt

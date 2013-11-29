c006372e72947cd3b22060ecda83513cefa160e9
Vladimir Zaytsev <zaytsev@usc.edu>
2013-11-29 01:54:15 -0800

    Test queries moved to a separate dir.

--- run_decompress.sh
+++ run_decompress.sh
@@ -1,29 +1,29 @@
 # Russian
 
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/ru/sentence.xz > test_data/tiny/ru/sentence.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/ru/sentence.xz > test_data/medium/ru/sentence.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/ru/sentence.xz > test_data/large/ru/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/tiny/ru/sentence.xz > test_data/tiny/ru/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/medium/ru/sentence.xz > test_data/medium/ru/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/large/ru/sentence.xz > test_data/large/ru/sentence.txt
 
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/ru/document.xz > test_data/tiny/ru/document.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/ru/document.xz > test_data/medium/ru/document.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/ru/document.xz > test_data/large/ru/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/tiny/ru/document.xz > test_data/tiny/ru/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/medium/ru/document.xz > test_data/medium/ru/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/large/ru/document.xz > test_data/large/ru/document.txt
 
 # Spanish
 
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/es/sentence.xz > test_data/tiny/es/sentence.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/es/sentence.xz > test_data/medium/es/sentence.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/es/sentence.xz > test_data/large/es/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/tiny/es/sentence.xz > test_data/tiny/es/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/medium/es/sentence.xz > test_data/medium/es/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/large/es/sentence.xz > test_data/large/es/sentence.txt
 
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/es/document.xz > test_data/tiny/es/document.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/es/document.xz > test_data/medium/es/document.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/es/document.xz > test_data/large/es/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/tiny/es/document.xz > test_data/tiny/es/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/medium/es/document.xz > test_data/medium/es/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/large/es/document.xz > test_data/large/es/document.txt
 
 # English
 
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/en/sentence.xz > test_data/tiny/en/sentence.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/en/sentence.xz > test_data/medium/en/sentence.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/en/sentence.xz > test_data/large/en/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/tiny/en/sentence.xz > test_data/tiny/en/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/medium/en/sentence.xz > test_data/medium/en/sentence.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/large/en/sentence.xz > test_data/large/en/sentence.txt
 
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/en/document.xz > test_data/tiny/en/document.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/en/document.xz > test_data/medium/en/document.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/en/document.xz > test_data/large/en/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/tiny/en/document.xz > test_data/tiny/en/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/medium/en/document.xz > test_data/medium/en/document.txt
+7za e -an -m0=lzma2 -txz -mmt=8 -si -so < test_data/large/en/document.xz > test_data/large/en/document.txt

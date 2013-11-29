99275e71b2e8ad9dbef2d10b0f69a1d45a98f3d8
Vladimir Zaytsev <zaytsev@usc.edu>
2013-11-28 23:10:34 -0800

    Updated test data and scripts. Removed junk data.

--- run_compress.sh
+++ run_compress.sh
@@ -1,23 +1,29 @@
 # Russian
 
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/ru/sentence.txt > test_data/tiny/ru/sentence.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/ru/sentence.txt > test_data/medium/ru/sentence.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/ru/sentence.txt > test_data/large/ru/sentence.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/tiny/ru/sentence.txt > test_data/tiny/ru/sentence.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/medium/ru/sentence.txt > test_data/medium/ru/sentence.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/large/ru/sentence.txt > test_data/large/ru/sentence.xz
 
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/ru/document.txt > test_data/tiny/ru/document.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/ru/document.txt > test_data/medium/ru/document.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/ru/document.txt > test_data/large/ru/document.xz
-
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/ru/triplet.txt > test_data/tiny/ru/triplet.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/ru/triplet.txt > test_data/medium/ru/triplet.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/ru/triplet.txt > test_data/large/ru/triplet.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/tiny/ru/document.txt > test_data/tiny/ru/document.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/medium/ru/document.txt > test_data/medium/ru/document.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/large/ru/document.txt > test_data/large/ru/document.xz
 
 # Spanish
 
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/es/sentence.txt > test_data/tiny/es/sentence.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/es/sentence.txt > test_data/medium/es/sentence.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/es/sentence.txt > test_data/large/es/sentence.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/tiny/es/sentence.txt > test_data/tiny/es/sentence.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/medium/es/sentence.txt > test_data/medium/es/sentence.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/large/es/sentence.txt > test_data/large/es/sentence.xz
 
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/es/document.txt > test_data/tiny/es/document.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/es/document.txt > test_data/medium/es/document.xz
-7z a -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/es/document.txt > test_data/large/es/document.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/tiny/es/document.txt > test_data/tiny/es/document.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/medium/es/document.txt > test_data/medium/es/document.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/large/es/document.txt > test_data/large/es/document.xz
+
+# English
+
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/tiny/en/sentence.txt > test_data/tiny/en/sentence.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/medium/en/sentence.txt > test_data/medium/en/sentence.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/large/en/sentence.txt > test_data/large/en/sentence.xz
+
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/tiny/en/document.txt > test_data/tiny/en/document.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/medium/en/document.txt > test_data/medium/en/document.xz
+7z a -an -m0=lzma2 -txz -mx=9 -mmt=8 -si -so < test_data/large/en/document.txt > test_data/large/en/document.xz
--- run_decompress.sh
+++ run_decompress.sh
@@ -8,10 +8,6 @@
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/ru/document.xz > test_data/medium/ru/document.txt
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/ru/document.xz > test_data/large/ru/document.txt
 
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/ru/triplet.xz > test_data/tiny/ru/triplet.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/ru/triplet.xz > test_data/medium/ru/triplet.txt
-7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/ru/triplet.xz > test_data/large/ru/triplet.txt
-
 # Spanish
 
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/es/sentence.xz > test_data/tiny/es/sentence.txt
@@ -21,3 +17,13 @@
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/es/document.xz > test_data/tiny/es/document.txt
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/es/document.xz > test_data/medium/es/document.txt
 7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/es/document.xz > test_data/large/es/document.txt
+
+# English
+
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/en/sentence.xz > test_data/tiny/en/sentence.txt
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/en/sentence.xz > test_data/medium/en/sentence.txt
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/en/sentence.xz > test_data/large/en/sentence.txt
+
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/tiny/en/document.xz > test_data/tiny/en/document.txt
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/medium/en/document.xz > test_data/medium/en/document.txt
+7za e -an -m0=lzma2 -txz -mx=4 -mmt=8 -si -so < test_data/large/en/document.xz > test_data/large/en/document.txt
